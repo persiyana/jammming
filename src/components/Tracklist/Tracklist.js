c85a254a54844966e9d4a4ff5b41e4dfc5722333
persiyana <persiyanageorgieva@gmail.com>
2023-07-16 17:54:51 +0300

    adding html and css from the project task

---
+++ src/components/Tracklist/Tracklist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TrackList.css';
+import {Track} from '../Track/Track';
+
+export class TrackList extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+                
+            </div>
+        );
+    }
+
+}
